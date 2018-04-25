d3b84e20d973d6eb278c56ee00d8123153fcbd7a
Ehron Gwinn <ehron@local>
2018-04-25 12:29:04 -0400

    fix local testing script

--- test.sh
+++ test.sh
@@ -4,20 +4,33 @@
 
 # set up the queue
 echo "Starting local Rabbitmq container..."
-if ! docker start benchmark-rabbit 2>&1  /dev/null; then
-    docker run -d -p 5672:5672 --hostname my-rabbit --name benchmark-rabbit rabbitmq:3
-fi
+docker start benchmark-rabbit || docker run -d -p 5672:5672 \
+    --hostname my-rabbit --name benchmark-rabbit rabbitmq:3
 
-# set up Vault
-echo "Starting local Vault server..."
-vault server -dev -dev-root
+# set up database for results
+echo "Creating local DB for to store results"
+mkdir -p ~/benchmark-data
+docker start mysql-benchmark || docker run --name mysql-benchmark \
+    -p 3306:3306 \
+    -v ~/benchmark-data:/var/lib/mysql \
+    -e MYSQL_ROOT_PASSWORD=root \
+    -e MYSQL_ROOT_HOST=% \
+    -e MYSQL_DATABASE=benchmark \
+    -e MYSQL_USER=vault \
+    -e MYSQL_PASSWORD=vaultpw \
+    -d mysql/mysql-server:5.7
+
 
 # Run the vault server
 echo "Starting local Vault server..."
 vault server -dev -dev-root-token-id=root &
 
 # Set the vault address environment variable
-export VAULT_ADDR='http://127.0.0.1:8200'
+export VAULT_ADDR=http://127.0.0.1:8200
+
+# auth
+#echo "Logging in with root token"
+vault login root
 
 # enable the transit secret engine
 echo "Enabling the transit engine"
@@ -26,17 +39,3 @@ vault secrets enable transit
 # create an encryption key to use for transit
 echo "Creating a key named \"benchmark\""
 vault write -f transit/keys/benchmark
-
-# set up database for results
-echo "Creating local DB for to store results"
-docker pull mysql/mysql-server:5.7
-mkdir ~/benchmark-data
-docker run --name mysql-benchmark \
-  -p 3306:3306 \
-  -v ~/benchmark-data:/var/lib/mysql \
-  -e MYSQL_ROOT_PASSWORD=root \
-  -e MYSQL_ROOT_HOST=% \
-  -e MYSQL_DATABASE=benchmark \
-  -e MYSQL_USER=vault \
-  -e MYSQL_PASSWORD=vaultpw \
-  -d mysql/mysql-server:5.7
